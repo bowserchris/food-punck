9f6bce9597e480168be729bb90a25cff70384c05
wandeavouros <wandeavouros@wandeavouros>
2025-07-24 17:15:03 +0200

    Uploading local project to github

---
+++ Food_Punck/src/main/java/org/ironhack/Food_Punck/FoodPunckApplication.java
@@ -0,0 +1,13 @@
+package org.ironhack.Food_Punck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPunckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodPunckApplication.class, args);
+	}
+
+}
---
+++ Food_Punck/src/test/java/org/ironhack/Food_Punck/FoodPunckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ironhack.Food_Punck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPunckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
