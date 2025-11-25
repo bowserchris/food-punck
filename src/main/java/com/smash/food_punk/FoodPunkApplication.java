d5e76556ff0d92c2b3e10be3aefa369d84b5e487
bowserchris <coding.chris@proton.me>
2025-11-26 00:41:02 +0100

    Starting project from scratch. New init

---
+++ src/main/java/com/smash/food_punk/FoodPunkApplication.java
@@ -0,0 +1,13 @@
+package com.smash.food_punk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodPunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smash/food_punk/FoodPunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smash.food_punk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
