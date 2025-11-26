f44e99510fab7bba54a9381a2352a798709f39a7
bowserchris <coding.chris@proton.me>
2025-11-26 20:44:13 +0100

    products

--- src/main/java/com/smash/food_punk/models/Product.java
+++ src/main/java/com/smash/food_punk/models/Product.java
@@ -3,9 +3,19 @@
  */
 package com.smash.food_punk.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
- * 
+ * Class representing a product or item
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
+
+	private String name;
+	private float price;
 
 }
