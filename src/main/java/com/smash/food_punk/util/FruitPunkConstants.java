e2b6cf9c0de6f2d96f37651ee2f9ddb8621886e0
bowserchris <coding.chris@proton.me>
2025-11-26 20:34:39 +0100

    Create constant package and class

---
+++ src/main/java/com/smash/food_punk/util/FruitPunkConstants.java
@@ -0,0 +1,15 @@
+/**
+ * @author bowserchris
+ */
+package com.smash.food_punk.util;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * Constants class for the Fruit Punk app
+ */
+@NoArgsConstructor
+public class FruitPunkConstants {
+
+
+}
